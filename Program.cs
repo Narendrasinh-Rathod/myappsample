73fb18018a8fcfe0341713169e993cf109c58dcb
Narendra Rathod <narendra.rathod@ecubix.com>
2025-07-30 18:38:17 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine("Hello, World!");
+
+// Random dice = new Random();
+// int roll = dice.Next(1,7);
+// Console.WriteLine($"Dice roll random value {roll}");
+
+// Console.Clear();
+
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50,101);
+
+// Console.WriteLine($"Dice roll1 random value {roll1}");
+// Console.WriteLine($"Dice roll2 random value {roll2}");
+// Console.WriteLine($"Dice roll3 random value {roll3}");
+
+int FirstValue = 500;
+int SecondValue = 600;
+int LargeValue;
+
+LargeValue = Math.Max(FirstValue,SecondValue);
+
+Console.WriteLine(LargeValue);
